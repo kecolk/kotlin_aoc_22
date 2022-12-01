63756da3aacaf70541f6e01cdbc53f0487c0a9d3
Pavel Klocek <klocek.pavel@feg.eu>
2022-12-01 07:23:02 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun elvesCalories(input: List<Int?>): Collection<Int> {
+        return input
+            .scan(
+                Pair<Int, Int?>(
+                    0,
+                    0
+                )
+            ) { acc, value -> Pair(if (value == null) acc.first + 1 else acc.first, value) }
+            .groupingBy { it.first }
+            .fold(0) { acc, element -> element.second?.let { acc + it } ?: acc }
+            .values
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<Int?>): Int = elvesCalories(input).max()
+
+    fun part2(input: List<Int?>): Int =
+        elvesCalories(input).sorted().takeLast(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import java.security.MessageDigest
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+    .readLines().map { it.toIntOrNull() }
 
 /**
  * Converts string to md5 hash.
